0efda98dc253e71bf64794874560680e28f5f00d
Mohamed Gamal <mgtm.prog@gmail.com>
2020-05-13 02:15:13 +0200

    Almost Finished this class, Needs to be tested once other classes is done

--- Controllers/memory_controller.cpp
+++ Controllers/memory_controller.cpp
@@ -1,1 +1,29 @@
 #include "memory_controller.h"
+
+MemoryController::MemoryController(unsigned long size, AllocatorType type){
+    mem = new Memory(size);
+    deallecator = new Deallocator(mem);
+    if(type == FIRST_FIT) allecator = new FirstFitAllocator(mem);
+    else allecator = new BestFitAllocator(mem);
+}
+
+
+bool MemoryController::allocateProcess(QString name, QVector<QString> &segmentsNames, QVector<unsigned long> &segmentsLimits){
+    bool out = allecator->allocateProcess(name, &segmentsNames, &segmentsLimits);
+}
+
+void MemoryController::deallocateSegment(unsigned long base, unsigned long limit){
+
+}
+
+void MemoryController::deallocateProcess(QString name){
+//    deallecator->deallocateProcess()
+}
+
+deque<Segment*>* MemoryController::getSegments(){
+    return mem->getSegments();
+}
+
+QVector<Process*>* MemoryController::getProcesses(){
+    return mem->getProcesses();
+}
--- Controllers/memory_controller.h
+++ Controllers/memory_controller.h
@@ -2,12 +2,32 @@
 #define MEMORY_CONTROLLER_H_
 
 #include "../Backend/memory.h"
+#include "../Backend/segment.h"
+#include "../Backend/allocator.h"
 #include "../Backend/first_fit_allocator.h"
 #include "../Backend/best_fit_allocator.h"
 #include "../Backend/deallocator.h"
+#include <QString>
+#include <QMap>
 
-class MemoryController
-{
+using std::deque;
+
+typedef enum AllocatorType{FIRST_FIT, BEST_FIT}AllocatorType;
+
+class MemoryController{
+public:
+    MemoryController(unsigned long size, AllocatorType type);
+    bool allocateProcess(QString name, QVector<QString> &segmentsNames, QVector<unsigned long> &segmentsLimits);
+    void deallocateSegment(unsigned long base, unsigned long limit);
+    void deallocateProcess(QString name);
+    deque<Segment*>* getSegments();
+    QVector<Process*>* getProcesses();
+
+private:
+    Memory *mem;
+    Allocator *allecator;
+    Deallocator *deallecator;
+    QMap <QString, Process*> processMap;
 };
 
 #endif // MEMORY_CONTROLLER_H_
