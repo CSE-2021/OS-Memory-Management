f3375dacc6db58048629e3b1722eb003be0db9d1
THOR <thor@localhost.localdomain>
2020-05-16 04:33:19 -0400

    v. 0

--- Backend/first_fit_allocator.cpp
+++ Backend/first_fit_allocator.cpp
@@ -6,7 +6,58 @@ FirstFitAllocator::FirstFitAllocator(Memory *memory):Allocator(memory)
 
 bool FirstFitAllocator::allocateProcess(QString processName, QVector<QString> *segmentsNames, QVector<unsigned long> *limits)
 {
+	//check if limit vector has same no of elements as segment names vector and not empty
+
+	if ( (segmentsNames->size() != limits->size() ) || (limits->size()==0 )  )
+          return false;
+      
+          
+    else{
+		
+			Qvector <int> holes ;
+			Qvector <int> hole_base ;
+			holes.pushback(0);
+		
+		// making avector of holes (size and index) pf current memory 
+		for (int i=0; i< memory->getSize() ;i++ ) 
+		{
+					//derefrencing pointer to deque of memory segments then getting the i's element
+					//   segment type to identify possible holes
+			if  ( (*(memory->getSegments()))[i].getSegmentType() == FREE ) 
+			
+			{
+			 (*holes.end())++ ;//increment hole space
+			 // save the index that marks the base of hole
+			 if (*holes.end()==1) hole_base.pushback(i); 
+			}
+			
+			 //if memory segment not free (end of ahole ) , add anew element for next hole
+			else if (*holes.end()!=0)holes.pushback(0);
+			
+		}
+			
+		/////////	
+		
+		// checking if all segments can fit in holes (first fit)	
+		for (int i=0; i< limits->size() ;i++ )
+		{
+				
+				
+		}
+			
+				
+		
+		
+		
+		
+				
+		}
+	
 }
+
+
+
+
 
 void FirstFitAllocator::allocateSegment(QString name, unsigned long limit)
 {
